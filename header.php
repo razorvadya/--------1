cabc0776e51ba434ea5536be2517099519e558f5
razorvadya <razorvadya@mail.ru>
2020-04-01 14:46:31 +0300

    init

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/vonlaine/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/vonlaine/slick/slick-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/vonlaine/css/body.css"/>
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+    <script src="/wp-content/themes/vonlaine/slick/slick.js" type="text/javascript" charset="utf-8"></script>
+
+	</head>
+    
+    
+<header>
+    <nav>
+        <img src="/wp-content/themes/vonlaine/img/logo.png">
+        <ul>
+            <li><a href="#menu-1">О продукте</a></li>
+            <li><a href="#menu-2">Возможности</a></li>
+            <li><a href="#menu-3">Уже используют</a></li>
+            <li><a href="#menu-4">Поставки</a></li>
+            <li><a href="#menu-5">Обучение</a></li>
+            <li><a href="#menu-6">Попробовать</a></li>
+        </ul>
+        <button class="header-nav-button-buy">Купить</button>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,263 @@
+<?php get_header(); ?>
+
+
+<section class="section-collage">
+    <article>
+        <div class="section-article-collage-img">
+            <div class="section-article-collage-blocks">
+                <div class="section-article-collage-text1">business studio</div>
+                <div class="section-article-collage-text2">система <br>бизнес-моделирования</div>
+            </div>
+        </div>
+    </article>
+</section>
+<section>
+    <article class="section-article-next-step">
+        <div class="section-article-next-step">Следующая ступень развития<br> Вашего бизнеса</div>   
+        <div class="section-article-next-step-info">Business Studio позволяет упорядочить вашу компанию и перевести ее на новый уровень развития. Уровень точной, скурпулёзно настроенной и отлаженной системы. Системы хорошо упорядоченной и способоной работать самостоятельно, без участия руководителя.</div>
+    </article>
+    <article id="menu-1" class="article-key">
+        <h2 class="article-key-h2"><div class="article-key-h2-div">
+			
+			Ключевые признаки системы <br>компании</div></h2>
+        <div class="article-key-blocks">
+            <div class="article-key-blocks-order">
+                <div class="article-key-blocks-order-img"></div>
+                <div class="article-key-blocks-order-title">Упорядоченность</div>
+                <div class="article-key-blocks-order-text">Каждый сотрудник компании точно знает какие у его компании цели, за что конкретно в компании он отвечает и как именно будут оцениваться результаты его работы.</div>
+            </div> 
+            <div class="article-key-blocks-controll">
+                <div class="article-key-blocks-controll-img"></div>
+                <div class="article-key-blocks-controll-title">Управляемость</div>
+                <div class="article-key-blocks-controll-text">Руководители и сотрудники компании могут видеть реальную картину происходящего в компании в настоящее время и влияют на нее в режиме реального времени. Компания предсказуемо реагирует на управляющие сигналы и разворачивается в нужном направлении.</div>
+            </div> 
+            <div class="article-key-blocks-product">
+                <div class="article-key-blocks-product-img"></div>
+                <div class="article-key-blocks-product-title">Продуктивность</div>
+                <div class="article-key-blocks-product-text">В компании нет функций, которые не добавляют никакой ценности, часто выполняются со сбоями или дублируются. Процессы компании организованы оптимально и непрерывно совершенствуются.</div>
+            </div> 
+        </div>
+    </article>
+    <article class="article-express-diag">
+        <div class="article-express-diag-item">
+            <div class="article-express-diag-item-text">
+                Проверить, насколько эффективно организована система управления в вашей компании сейчас и решить может ли быть вам полезна система организационного проектирования «Business Studio» можно с помощью нашей анкеты экспресс-диагностики.
+            </div>
+            <button class="article-express-diag-item-button">Скачать анкету экспресс-диагностики</button>
+        </div>
+    </article>
+</section>
+<section class="capability">
+    <article id="menu-2" class="article-capability">
+        <div class="article-capability-title">Возможности</div>
+        <div class="article-capability-blocks">
+            <div class="article-capability-blocks-text">
+                В Business Studio есть все, чтобы проработать все составные элементы системы управления организации, а так же контролировать ее работоспособность и эффективность
+            </div>
+            <div class="article-capability-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/cap-img-1.png">
+                <div class="article-capability-blocks-items-text">Постановка стратегических целей и ключевых показателей компании</div>
+            </div>
+            <div class="article-capability-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/cap-img-2.png">
+                <div class="article-capability-blocks-items-text">Разработка оптимальной модели бизнес-процессов компании</div>
+            </div>
+            <div class="article-capability-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/cap-img-3.png">
+                <div class="article-capability-blocks-items-text">Проектирование организационной структуры, уместной для вашей компании</div>
+            </div>
+            <div class="article-capability-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/cap-img-4.png">
+                <div class="article-capability-blocks-items-text">Оптимизация скорости и стоимости выполнения бизнес-процессов компании</div>
+            </div>
+            <div class="article-capability-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/cap-img-5.png">
+                <div class="article-capability-blocks-items-text">Стандартизация бизнес-процессов и регламентация должностных обязанностей сотрудников компании</div>
+            </div>
+            <div class="article-capability-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/cap-img-6.png">
+                <div class="article-capability-blocks-items-text">Возможность контроля, анализа и управления бизнес-процессами в режиме реального времени</div>
+            </div>
+            <div class="article-capability-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/cap-img-7.png">
+                <div class="article-capability-blocks-items-text">Автоматизация бизнес-процессов и внедрение информационных систем</div>
+            </div>
+        </div>
+    </article>
+</section>
+<section>
+    <article id="menu-3" class="article-trust">
+        <div class="article-trust-title">Компании, которые уже используют <br>Business Studio</div>  
+        <section class="lazy slider" >
+            <div class="article-trust-slider-block-1">
+                <div class="article-trust-slider-block-1-photo"></div>
+                <div class="article-trust-slider-block-1-name">Оксана Борсук</div>
+                <div class="article-trust-slider-block-1-post">Аналитик бизнес-процессов</div>
+                <div class="article-trust-slider-block-1-company"></div>
+                <div class="article-trust-slider-block-1-text">Предпослыкой внедрения программы была необходимость описания бизнес-модели одного из направлений компании. Основная цель, которую мы для себя ставили - подробное, пошаговое описание товарного движения. В результате внедрения программы описан целый ряд важных процессов, в которых наблюдался сбой. Особо отметили удобство программы в работе при визуализации в доступной форме действующий бизнес-процессов, нахождения «узких мест» в процессах.</div>
+            </div>
+            <div class="article-trust-slider-block-2">
+                <div class="article-trust-slider-block-2-photo"></div>
+                <div class="article-trust-slider-block-2-name">Оксана Борсук</div>
+                <div class="article-trust-slider-block-2-post">Аналитик бизнес-процессов</div>
+                <div class="article-trust-slider-block-2-company"></div>
+                <div class="article-trust-slider-block-2-text">Предпослыкой внедрения программы была необходимость описания бизнес-модели одного из направлений компании. Основная цель, которую мы для себя ставили - подробное, пошаговое описание товарного движения. В результате внедрения программы описан целый ряд важных процессов, в которых наблюдался сбой. Особо отметили удобство программы в работе при визуализации в доступной форме действующий бизнес-процессов, нахождения «узких мест» в процессах.</div>
+            </div>
+        </section>
+        <script type="text/javascript">$(document).on('ready', function() {$(".lazy").slick({lazyLoad: 'ondemand', infinite: true});});</script>
+    </article>
+</section>
+<section>
+    <article id="menu-4" class="article-delopt">
+        <div class="article-delopt-title">Варианты поставки</div>
+        <div class="article-delopt-text">Существует два варианта поставки системы бизнес-моделирования <br>Business Studio 4.0</div>
+        <div class="article-delopt-slider">
+            <div class="article-delopt-slider-item article-delopt-slider-item-b">Общее описание</div>
+            <div class="article-delopt-slider-item">Функционал и стоимость</div>
+            <div class="article-delopt-slider-item">Требования к установке</div>
+            <div class="article-delopt-slider-item">Дополнительные модули</div>
+        </div>
+        <div class="article-delopt-blocks-b-g">
+            <div class="article-delopt-blocks-b">
+                <div class="article-delopt-blocks-b-buy">
+                    <div class="article-delopt-blocks-b-buy-title">Business Studio <span>Professional</span></div>
+                    <button class="article-express-diag-item-button article-delopt-blocks-b-buy-button">Купить</button>
+                </div>
+                <div class="article-delopt-blocks-b-reccomend">
+                    <div class="article-delopt-blocks-b-reccomend-text">Рекомендуем <br>для малого и среднего бизнеса</div>
+                    <div class="article-delopt-blocks-b-reccomend-after-before">
+                        <img src="/wp-content/themes/vonlaine/img/slider-rec.png">
+                        <div class="article-delopt-blocks-b-reccomend-after-before-block">
+                            <div class="article-delopt-blocks-b-reccomend-after">от <span>&nbsp;100</span></div>
+                            <div class="article-delopt-blocks-b-reccomend-before">до <span>&nbsp;5000&nbsp;</span> сотрудников</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="article-delopt-blocks-b-g-advantages-blocks">
+                    <div class="article-delopt-blocks-b-g-advantages-head">Business Sudio Professional - это базовый вариант поставки программы, который позволяет решить все основные задачи, стоящие перед бизнесом на начальном этапе наведения порядка и перехода к системному управлению.</div>
+                    <div class="article-delopt-blocks-b-g-advantages-text">С помощью Business Studio Professional вы сможете:
+                        <br><br>1. разработать модель бизнес-процессов компании;
+                        <br><br>2. создать документы, регламентирующие правила выполнения бизнес-процессов сотрудниками (процедуры управления);
+                        <br><br>3. формализовать организационную структуры компании;
+                        <br><br>4. в «один клик» сформировать должностные инструкции, в которых будет четко и конкретно написано, что именно и как должен делать каждый сотрудник в рамках бизнес-процессов, в которых он принимает участие;
+                        <br><br>5. разработать систему ключевых показателей эффективности бизнес-процессов и внедрить автоматческий контроль показателей работы сотрудников;
+                        <br><br>6. формализовать цели компании и стратегию их достижения;
+                        <br><br>7. создать финансовую структуру и регламентировать систему бюджетирования компании. 
+                    </div>
+                </div>
+            </div>
+            <div class="article-delopt-blocks-g">
+                <div class="article-delopt-blocks-g-buy">
+                    <div class="article-delopt-blocks-g-buy-title">Business Studio <span>Enterprise</span></div>
+                    <button class="article-express-diag-item-button article-delopt-blocks-g-buy-button">Купить</button>
+                </div>
+                <div class="article-delopt-blocks-g-reccomend">
+                    <div class="article-delopt-blocks-g-reccomend-text">Рекомендуем <br>для крупного бизнеса</div>
+                    <div class="article-delopt-blocks-g-reccomend-after-before">
+                        <img src="/wp-content/themes/vonlaine/img/slider-rec-g.png">
+                        <div class="article-delopt-blocks-g-reccomend-after-before-block">
+                            <div class="article-delopt-blocks-g-reccomend-after">Более <span>&nbsp;5000&nbsp;</span>сотрудников</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="article-delopt-blocks-b-g-advantages-blocks">
+                    <div class="article-delopt-blocks-b-g-advantages-head">Business Studio Enterprise - это расширенный вариант поставки программы, который позволит решить, помимо основных задач, сложные вопросы математической оптимизации бизнес-процессов, внедрения системы управления качеством и подготовки компании к автоматизации бизнес-процессов.</div>
+                    <div class="article-delopt-blocks-b-g-advantages-text">С помощью Business Studio Professional вы сможете:
+                        <br><br>1. оптмизировать бизнес-процессы компании используя инструментарий имитационного моделирования и функционально-стоимостного анализа;
+                        <br><br>2. разработать и внедрить систему правления качеством согласно ISO 9001;
+                        <br><br>3. построить систему контроллинга исполения бизнес-процессов;
+                        <br><br>4. сформулировать тезнические задания на автоматизацию бизнес-процессов;
+                        <br><br>5. интегрировать модели бизнес-процессов с BPM-системой Directum.
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </article>
+</section>
+<h2 class="compare-options-h2">
+    <img src="/wp-content/themes/vonlaine/img/left-right-ar.png">
+    <div class="compare-options-h2-text">Сравнить варианты</div>
+</h2>
+<section>
+    <article class="article-additional-modules">
+        <div class="article-additional-modules-title">Обратите внимание на дополнительные <br>модули</div>
+        <div class="article-additional-modules-blocks">
+            <div class="article-additional-modules-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/addit-modules-1.png">
+                <div class="article-additional-modules-blocks-items-title">Business Studio 4.0 Portal</div>
+                <div class="article-additional-modules-blocks-items-text">Интернет-портал, позволяющий оперативно обновлять базу знаний компании, содержащую всю необходимую для работы сотрудников регламентирующую документацию и отчеты.</div>
+                <a class="article-additional-modules-blocks-items-a" href="#">Подробнее</a>
+            </div>
+            <div class="article-additional-modules-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/addit-modules-2.png">
+                <div class="article-additional-modules-blocks-items-title">Business Studio 4.0 Cockpit</div>
+                <div class="article-additional-modules-blocks-items-text">Редакция Business Studio с ограниченным функционалом, предназначена для ввода, просмотра, контроля и анализа информации в Business Studio.</div>
+                <a class="article-additional-modules-blocks-items-a" href="#">Подробнее</a>
+            </div>
+            <div class="article-additional-modules-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/addit-modules-3.png">
+                <div class="article-additional-modules-blocks-items-title">MetaEdit</div>
+                <div class="article-additional-modules-blocks-items-text">Редактор для внесения изменений в структуры классов баз данных Business Studio.</div>
+                <a class="article-additional-modules-blocks-items-a" href="#">Подробнее</a>
+            </div>
+            <div class="article-additional-modules-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/addit-modules-4.png">
+                <div class="article-additional-modules-blocks-items-title">Апргейды</div>
+                <div class="article-additional-modules-blocks-items-text">Редакция Business Studio с ограниченным функционалом, предназначена для ввода, просмотра, контроля и анализа информации в Business Studio.</div>
+                <a class="article-additional-modules-blocks-items-a" href="#">Подробнее</a>
+            </div>
+        </div>
+    </article>
+</section>
+<section>
+    <article id="menu-5" class="article-teaching">
+        <div class="article-teaching-title">Обучение</div>
+        <div  class="article-teaching-block">
+            <div class="article-teaching-block-text-h1">Большой опыт во внедрении комплексных проектов позволяет нам форматировать
+                программу обучения в соответствии с запросом компании-клиента таким образом,
+                чтобы обучение было эффективным и для начинающих и для опытных
+                специалистов. В ходе обучения эксперты нашей компании дадут емкий
+                мастер-класс о настройке и управлении бизнес-процессами вместе с программой
+                Business Studio (и 0, а так же расскажут о ее базовых и продвинутых возможностях.
+            </div>
+            <div class="article-teaching-block-text"><br>Обучение проводится в корпоративном или индивидуальном формате на рабочих местах пользователей
+                программного продукта. Количество людей в группе может быть любым, равным количеству
+                сотрудников, которые в будущем будут использовать программный продукт в своей работе.
+                Рекомендуемый объем программы - 2 дня (16 академических часов).
+
+                После окончания обучения выдается сертификат от компании «Актив-Система». Так же, после
+                прохождения сертификационного теста, слушатели могут получить статус сертифицированного
+                специалиста непосредственно от разработчика продукта.<br><br>
+                Как правило, мы адаптируем типовую программу обучения под конкретный запрос наших клиентов,
+                поэтому окончательная стоимость обучения определяется после согласования программы и объема
+                обучения.
+            </div>
+            <button class="article-teaching-block-button article-express-diag-item-button">Отправить заявку</button>
+        </div>
+    </article>
+</section>
+<section>
+    <article id="menu-6"  class="article-to-try">
+        <div class="article-to-try-title">Попробовать</div>
+        <div class="article-to-try-blocks">
+            <div class="article-to-try-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/try-img-1.png">
+                <div class="article-to-try-blocks-items-add">
+                    <div class="article-to-try-blocks-text">Демо-версия предоставляет протестировать функционал программы и посмотреть проект модели бизнес-процессов на примере компании «Интехпроект»</div>
+                    <a class="article-to-try-blocks-a" href="">Получить ссылку на демо-версию</a>
+                </div>
+            </div>
+            <div class="article-to-try-blocks-items">
+                <img src="/wp-content/themes/vonlaine/img/try-img-2.png">
+                <div class="article-to-try-blocks-items-add">
+                    <div class="article-to-try-blocks-text">Полная версия с ограниченным сроком действия позволяет протестировать на практике весь функционал программы без ограничений. После согласования заявки нашим консультантом, предосталвяется доступ к лицензии на период 14 календарных дней.</div>
+                    <a class="article-to-try-blocks-a" href="">Отправить заявку</a>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
+<button class="buy-business-studio">Купить Business Studio</button>
+<?php
+get_footer();
